ec1c177a6196767ad5877af19a9a494c1c9887ea
Chaltu Hassan <chaltu11hassan@gmail.com>
2022-07-27 12:42:29 -0500

    ready for deployment

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const exp = require('constants');
+
+const app = express();
+
+app.get('/', function(res, req){
+    res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+const port = process.env.PORT || 4000
